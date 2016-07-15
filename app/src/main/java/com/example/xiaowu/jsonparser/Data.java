d0bde28d090c028ca47b1d652698ce04cd6511bf
matingting <git@github.com:MaXiaoWu2015/MaXiaoWU.git>
2016-07-15 15:49:08 +0800

    test

--- app/src/main/java/com/example/xiaowu/jsonparser/Data.java
+++ app/src/main/java/com/example/xiaowu/jsonparser/Data.java
@@ -5,14 +5,12 @@ import java.util.ArrayList;
 /**
  * Created by xiaowu on 2016-7-13.
  */
-public class Data {
-    private ArrayList<Message> messages;
-
-    public ArrayList<Message> getMessages() {
-        return messages;
+public class Data <T>{
+    private ArrayList<T> list;
+    public ArrayList<T> getMessages() {
+        return list;
     }
-
-    public void setMessages(ArrayList<Message> messages) {
-        this.messages = messages;
+    public void setMessages(ArrayList<T> messages) {
+        this.list = messages;
     }
 }
